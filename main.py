e61932fe7e8db90e6ee7cfb045d07e520b3513d5
Yhoimer Salas <choy2324@gmail.com>
2023-04-22 21:08:20 -0500

    crea funcion que suma 5 numeros de una lista

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import problema1
+
 
 '''
 tres comillas sirven para comentar
@@ -47,3 +49,7 @@ if __name__ == '__main__':
     print(lista1)
 
     print(lista1[3])
+
+    print()
+
+    problema1.sumar_5_enteros()
